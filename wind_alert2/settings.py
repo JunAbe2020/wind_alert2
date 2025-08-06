290b72dc61fce4783d2789d4828c8503071e9e53
jun <aaabjny@gmail.com>
2025-08-06 15:56:38 +0900

    Update SECRET_KEY to use environment variable and add DEBUG/ALLOWED_HOSTS settings

--- wind_alert2/settings.py
+++ wind_alert2/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '5ea1mch@q9j#(v98jz-zt)9)w8s0(^va($sa$z*(2$3p8z(^!j')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-5ea1mch@q9j#(v98jz-zt)9)w8s0(^va($sa$z*(2$3p8z(^!j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
